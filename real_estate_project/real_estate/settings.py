232cc372977cb1f874d721ace9a57ecca4b5cc30
lanusky <lantusky2017@gmail.com>
2019-01-15 15:42:38 -0800

    remove secrets in settings.py

--- real_estate_project/real_estate/settings.py
+++ real_estate_project/real_estate/settings.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k_!3c25!#)(wm8_*z3vwy-8xwbdj^q7ny17c$76%xrf69ozdw@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,21 +61,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'real_estate.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'realestatedb',
-        'USER': 'postgres',
-        'PASSWORD': 'lt1994615',
-        'HOST': 'localhost'
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
