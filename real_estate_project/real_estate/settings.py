b67e05d9f6ce0f1d4d39fdbbe9efc0f8ea4e169f
lanusky <lantusky2017@gmail.com>
2019-01-14 16:56:08 -0800

    commit

--- real_estate_project/real_estate/settings.py
+++ real_estate_project/real_estate/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,3 +135,15 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+# Email Settings
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'auto.tulan@gmail.com'
+# EMAIL_HOST_PASSWORD = 'lt1994615'
+# EMAIL_USE_TLS = True
