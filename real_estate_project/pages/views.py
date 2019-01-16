9feb26ce4ec6e2f599c6e3a44b900785bb1ac97d
lanusky <lantusky2017@gmail.com>
2019-01-16 10:58:32 -0800

    change status to under construction

--- real_estate_project/pages/views.py
+++ real_estate_project/pages/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
-from listings.choices import state_choices, price_choices, bedroom_choices 
+from listings.choices import state_choices, price_choices, bedroom_choices
 
-# Create your views here.
+
 def index(request):
-  listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-  context = {
-    'listings': listings,
-    'state_choices': state_choices,
-    'price_choices': price_choices,
-    'bedroom_choices': bedroom_choices,
-  }
-  return render(request, 'pages/index.html', context)
+    return HttpResponse('<h1>Sites Under Construction!</h1>')
+    # listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    # context = {
+    #   'listings': listings,
+    #   'state_choices': state_choices,
+    #   'price_choices': price_choices,
+    #   'bedroom_choices': bedroom_choices,
+    # }
+    # return render(request, 'pages/index.html', context)
+
 
 def about(request):
-  # get all realtors
-  realtors = Realtor.objects.order_by('-hire_date')
-  # get all mvps
-  mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
-  
-  context = {
-    'realtors': realtors,
-    'mvp_realtors': mvp_realtors
-  }
-  return render(request, 'pages/about.html', context)
+    # get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    # get all mvps
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
